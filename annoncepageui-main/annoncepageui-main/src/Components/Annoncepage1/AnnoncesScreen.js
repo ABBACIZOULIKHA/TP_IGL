3a8a6fe1934851377c53be46eab2a38fe92da385
ABBACIZOULIKHA <121138480+ABBACIZOULIKHA@users.noreply.github.com>
2023-01-10 20:36:33 +0100

    Update AnnoncesScreen.js

--- annoncepageui-main/annoncepageui-main/src/Components/Annoncepage1/AnnoncesScreen.js
+++ annoncepageui-main/annoncepageui-main/src/Components/Annoncepage1/AnnoncesScreen.js
@@ -5,20 +5,7 @@ import { useState } from 'react'
 import { useEffect } from 'react'
 
 function AnnoncesScreen(){
-  // const annances =[
-  //   {
-  //     'titre': 'fjfjfj',
-  //     'prix':'44747',
-  //     'commune': 'CJCJC',
-  //     'date': '12/1/2023'
-  //   },
-  //   {
-  //     'titre': 'kffkfkf',
-  //     'prix':'272',
-  //     'commune':'FKJFKF' ,
-  //     'date': '12/1/2023'
-  //   }
-  //]
+  
    const [Annances,setAnnacses]=useState([])
 
    useEffect(()=> {
@@ -28,13 +15,12 @@ function AnnoncesScreen(){
 
    function fetchData(baseurl){
    fetch(baseurl)
-   .then(response => response.json)
-   .then((data) => setAnnacses(data.response)
-
+   .then(response => response.json())
+   .then((data) => setAnnacses(data)
    ) ;
   }
   return (
-    <div className="row mb-4 ">
+    <div className="AnnoncesC">
       { 
       Annances.map((annance) => <CardAnnonce annance={annance} />)
       }
